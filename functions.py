620288d6cbabd2699e86c60ac387030ebb0dce5f
mulirosantos <contato.msantos02@gmail.com>
2024-10-25 01:37:51 -0300

    adding modules

---
+++ functions.py
@@ -0,0 +1,29 @@
+from os import path, listdir
+
+
+class MyDir():
+    def __init__(self, diretorio):
+        self.diretorio = diretorio
+    
+    def listing(self):
+        try:
+            flist = listdir(self.diretorio)
+            return flist
+        except FileNotFoundError:
+            print(f"This directory doesn't exist! Check again")
+            
+class Files(MyDir):
+    def __init__(self, diretorio):
+        super().__init__(diretorio)
+    
+    def check_files(diretorio,flist):
+        try:
+            myFiles = [f for f in flist if path.isfile(path.join(diretorio,f))]
+            return myFiles                
+        except:
+            pass
+
+mydir = "d:/Users/Murilo/Documents/MURILO/sortFiles"
+listador = MyDir(mydir)
+
+print(listador.listing())
